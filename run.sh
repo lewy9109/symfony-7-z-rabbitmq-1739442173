866f8cbd59091ebaed30db92aa287074e8601c6b
Krystian Lewandowski <k.lewandowski@global4net.com>
2025-02-13 12:24:58 +0100

    Init project with gh-action

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(pwd)
+echo "Working directory: $BASEDIR"
+cp -n "$BASEDIR/.env.dist" "$BASEDIR/.env"
+
+
+docker-compose stop
+docker-compose build
+docker-compose up -d --remove-orphans
+
+docker exec -it --user www-data app_php composer install
+#docker exec -it --user www-data "${CONTAINER_NAME}" bin/console doctrine:database:create --if-not-exists
+#docker exec -it --user www-data "${CONTAINER_NAME}" bin/console doctrine:migrations:migrate -n
+#docker exec -it --user www-data "${CONTAINER_NAME}" bin/console doctrine:schema:validate
