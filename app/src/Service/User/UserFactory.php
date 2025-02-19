e40b748b620b88473d7fc6b28ad802f1b286b099
Krystian Lewandowski <k.lewandowski@global4net.com>
2025-02-19 09:26:25 +0100

    Fix php stan

--- app/src/Service/User/UserFactory.php
+++ app/src/Service/User/UserFactory.php
@@ -4,15 +4,19 @@ namespace App\Service\User;
 
 class UserFactory
 {
+    /**
+     * @param array<string, string> $data
+     */
     public static function fromArray(array $data): UserDto
     {
         $dto = new UserDto(
-            id: (int)$data['id'] ?? '',
-            fullName: $data['fullName'] ?? '',
-            email: $data['email'] ?? '',
-            city: $data['city'] ?? '',
-            reportId: $data['reportId'] ?? '',
+            id: isset($data['id']) ? (int)$data['id'] : null,
+            fullName: isset($data['fullName']) ? (string)$data['fullName'] : null,
+            email: isset($data['email']) ? (string)$data['email'] : null,
+            city: isset($data['city']) ? (string)$data['city'] : null,
+            reportId: isset($data['reportId']) ? (string)$data['reportId'] : null,
         );
+
 
         if (isset($data['fullName'])) {
             $dto->setFullName($data['fullName']);
